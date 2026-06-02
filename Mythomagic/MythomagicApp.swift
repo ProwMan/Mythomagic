9ee738db3274fefa896c8113f0ffe163fc632dcf
Madhan <selvakumarmadhan819@gmail.com>
2026-06-02 17:03:54 +0800

    Initial Commit

---
+++ Mythomagic/MythomagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MythomagicApp.swift
+//  Mythomagic
+//
+//  Created by Madhan on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MythomagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
